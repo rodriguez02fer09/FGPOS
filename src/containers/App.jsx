d1b8030875a093b314fca3e24f1b11a697c70d32
Fernanda Rodriguez <rodriguez02fer09@gmail.com>
2019-10-09 21:59:59 -0500

    initial base project

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../assets/styles/App.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>FGPOS</h1>
+    </div>
+  );
+};
+
+export default App;
