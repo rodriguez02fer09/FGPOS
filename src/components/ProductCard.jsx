55e61b7accd37831c5587b48b33232bd6b7c8044
Fernanda Rodriguez <rodriguez02fer09@gmail.com>
2019-11-14 21:28:30 -0500

    creacion modal

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -16,18 +16,60 @@ const customStyles = {
 };
 
 Modal.setAppElement("#app");
+class ProductCard extends React.Component {
+  constructor(props) {
+    super();
 
-const ProductCard = props => {
-  const { id, image, name, boughtUnits, soldUnits } = props;
+    this.state = {
+      modalIsOpen: false
+    };
 
-  return (
-    <div className="ProductCard">
-      <img src={image} alt={name} />
-      <h4>{name}</h4>
-      <h5>Unidades existentes: {boughtUnits - soldUnits}</h5>
-    </div>
-  );
-};
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({
+      modalIsOpen: true
+    });
+  }
+
+  afterOpenModal() {
+    console.log("afterOpenModal");
+  }
+
+  closeModal(e) {
+    e.stopPropagation();
+    this.setState({ modalIsOpen: false });
+  }
+
+  render() {
+    return (
+      <div onClick={this.openModal} className="ProductCard">
+        <img src={this.props.image} alt={this.props.name} />
+        <h4>{this.props.name}</h4>
+        <h5>
+          Unidades existentes: {this.props.boughtUnits - this.props.soldUnits}
+        </h5>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          customStyles={customStyles}
+          onAfterOpen={this.aftermodalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel="Example Modal"
+        >
+          <h2>{this.props.name}</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input value={this.props.name} />
+          </form>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = {
   addToCart
