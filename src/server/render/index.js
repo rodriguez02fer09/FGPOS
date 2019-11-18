bdfd99524c975ab6336103460a47aac8bce3fef6
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-11-17 21:52:22 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ src/server/render/index.js
@@ -0,0 +1,37 @@
+import getManifest from '../getManifest';
+
+let files = false;
+if (process.env.NODE_ENV !== 'development') files = getManifest();
+
+const render = (html, preloadedState) => {
+    return (`
+  <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta author="Fernanda Rodriguez & Gabriel Pinto">
+      <meta description="Sistema de ventas innovador y único">
+      <title>FG Pos</title>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" crossorigin="anonymous">
+      <link rel="stylesheet" href="${files ? files['main.css'] : 'assets/app.css'}" type="text/css"></link>
+    </head>
+    <body>
+      <div id="app">${html}</div>
+      <script>
+        // WARNING: See the following for security issues around embedding JSON in HTML:
+        // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+        window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+          /</g,
+          '\\u003c'
+        )}
+      </script>
+      <script src="${files ? files['main.js'] : 'assets/app.js'}" type="text/javascript"></script>
+      <script src="${files ? files['vendors.js'] : 'assets/vendor.js'}" type="text/javascript"></script>
+      </body>
+    </html>
+  `);
+};
+
+export default render;
