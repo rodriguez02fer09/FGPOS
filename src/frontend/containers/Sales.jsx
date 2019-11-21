b8692fd06f281ec2912a7cd32731831b6b7b86ac
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-11-21 18:16:59 -0500

    Sales details are ready

--- src/frontend/containers/Sales.jsx
+++ src/frontend/containers/Sales.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import '../assets/styles/containers/Sales.scss';
 import SalesCart from '../components/SalesCart';
 import ItemsList from '../components/ItemsList';
 import LogOut from '../components/Logout';
 
-class Sales extends React.Component {
-  render() {
-    return (
-      <>
-        <div id="salesItems">
-          <div id="salesItemsHeader" className="h-6 border-bottom bg-white">
-            <div />
-            <h3 className="mb-0">Ventas</h3>
-            <LogOut />
-          </div>
-          <ItemsList available={true} />
+const Sales = ({ user }) => {
+  return (
+    <>
+      <div id="salesItems">
+        <div id="salesItemsHeader" className="h-6 border-bottom bg-white">
+          <div />
+          <h3 className="mb-0">{user.name} - Ventas</h3>
+          <LogOut />
         </div>
-        <div id="salesCart">
-          <SalesCart />
-        </div>
-      </>
-    );
-  }
+        <ItemsList available={true} />
+      </div>
+      <div id="salesCart">
+        <SalesCart />
+      </div>
+    </>
+  );
 }
 
-export default Sales;
+const mapStateToProps = ({ user }) => {
+  return {
+    user
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  null,
+)(Sales);
