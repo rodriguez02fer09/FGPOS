5e00875d5b3e936d813f8a1262837f7676034a0f
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-11-26 20:18:37 -0500

    Eslint fixed

--- src/frontend/components/InvoicesList.jsx
+++ src/frontend/components/InvoicesList.jsx
@@ -3,45 +3,45 @@ import { connect } from 'react-redux';
 import '../assets/styles/components/InvoicesList.scss';
 import Invoice from './Invoice';
 
-const InvoicesList = ({ invoices, loading }) => {
-    return (
-        <>
-            {loading && (
-
-                <main id="invoicesLoadingContainer">
-                    <i className="fas fa-spin fa-spinner fa-2x" />
-                </main>
-            )}
-            {!loading &&
-                <>
-                    <h3 id="invoicesGeneratedTitle">Facturas generadas</h3>
-                    <main id="invoicesList">
-                        {invoices.map(invoice => (
-                            <Invoice key={invoice._id} creationDate={new Date(invoice.creationDate)} totalPrice={invoice.totalPrice} soldProducts={calculateSoldProducts(invoice.soldProducts)} />
-                        ))}
-                    </main>
-                </>
-            }
-        </>
-    );
+const calculateSoldProducts = (soldProducts) => {
+  let numberOfSoldProducts = 0;
+  soldProducts.forEach((product) => {
+    numberOfSoldProducts += product.soldUnits;
+  });
+  return numberOfSoldProducts;
 };
 
-const calculateSoldProducts = (soldProducts) => {
-    let numberOfSoldProducts = 0;
-    soldProducts.forEach(product => {
-        numberOfSoldProducts += product.soldUnits;
-    });
-    return numberOfSoldProducts;
+const InvoicesList = ({ invoices, loading }) => {
+  return (
+    <>
+      {loading && (
+
+        <main id="invoicesLoadingContainer">
+          <i className="fas fa-spin fa-spinner fa-2x" />
+        </main>
+      )}
+      {!loading && (
+        <>
+          <h3 id="invoicesGeneratedTitle">Facturas generadas</h3>
+          <main id="invoicesList">
+            {invoices.map(invoice => (
+              <Invoice key={invoice._id} creationDate={new Date(invoice.creationDate)} totalPrice={invoice.totalPrice} soldProducts={calculateSoldProducts(invoice.soldProducts)} />
+            ))}
+          </main>
+        </>
+      )}
+    </>
+  );
 };
 
 const mapStateToProps = ({ invoices, loading }) => {
-    return {
-        invoices,
-        loading
-    };
+  return {
+    invoices,
+    loading,
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    null
+  mapStateToProps,
+  null,
 )(InvoicesList);
